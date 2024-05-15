78d19e56000ed17ae2463ed967508fe7fb2b468a
Byron Gregorio Calderón López <eliashc9@gmail.com>
2024-05-14 22:53:33 -0500

    Corrida de la entidad Clientes - api de rest con c#
    se creo un nuevo proyecto para la creación de la api rest con c#, se creo el crud y se estableció la conexión con la db de la entidad clientes para hacer uso del postman o del visualizador que trae por defecto el asp.net core.

---
+++ proyectoElias/proyectoElias/Controllers/ClientesController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using proyectoElias.Data.Servicios;
+using proyectoElias.Model;
+
+namespace proyectoElias.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientesController : ControllerBase
+    {
+        private readonly IClientesServicios _clientesServicios;
+
+        public ClientesController(IClientesServicios clientesServicios)
+        {
+            _clientesServicios = clientesServicios;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetTodosClientes()
+        {
+            return Ok(await _clientesServicios.GetTodosClientes());
+        }
+
+        [HttpGet("{clientesId}")]
+        public async Task<IActionResult> GetClientesDetalles(int clientesId)
+        {
+            return Ok(await _clientesServicios.GetDetalles(clientesId));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CrearClientes(Clientes clientes)
+        {
+            if (clientes == null)
+                return BadRequest();
+            
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            
+            var created = await _clientesServicios.InsertClientes(clientes);
+
+            return Created("created", created);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateClientes(Clientes clientes)
+        {
+            if (clientes == null)
+                return BadRequest();
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            await _clientesServicios.UpdateClientes(clientes);
+
+            return NoContent();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteClientes(int clientesId)
+        {
+            await _clientesServicios.DeleteClientes(new Clientes { Cliente_id = clientesId });
+            return NoContent();
+        }
+    }
+}
