1ca6e97a45d6677713e28a1b25bc177041782fc0
Byron Gregorio Calderón López <eliashc9@gmail.com>
2024-05-15 09:57:48 -0500

    Creación del archivo para la conexión
    se realiza la conexión por medio de un archivo diferente para tener el src más limpio, este archivo llama al json y estos dos son llamados en el app

---
+++ proyectoElias/gimnasioElias.Data/Servicios/MySQLConfiguracion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gimnasioElias.Data.Servicios
+{
+    public class MySQLConfiguracion
+    {
+        //metodo para realizar la conexión a través del llamado de appsettings.json para las credenciaels de la db
+        public MySQLConfiguracion(string connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+
+        public string ConnectionString { get; set; }
+    
+    }
+}
