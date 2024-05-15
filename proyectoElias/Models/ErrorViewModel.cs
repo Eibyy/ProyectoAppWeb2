739a773b25e12d073e474834efc9cb29a999a95d
Byron Gregorio Calderón López <eliashc9@gmail.com>
2024-05-14 19:35:58 -0500

    Eliminación del branch de c# para realizarlo como api
    se eliminará la carpeta del proyecto por estar realizado como una aplicación web y no como una api rest para realizarlo nuevamente.

--- proyectoElias/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace gimnasioGestion.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
