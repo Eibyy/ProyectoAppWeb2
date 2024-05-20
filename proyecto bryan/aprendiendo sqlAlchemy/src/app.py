3e7a2f185b7fe681269df086cafb17f8da393e63
Bryan Bravo <bryanbr.0310@gmail.com>
2024-05-20 14:04:43 -0500

    practica sqlachemy y flask terminada

--- proyecto bryan/aprendiendo sqlAlchemy/src/app.py
+++ proyecto bryan/aprendiendo sqlAlchemy/src/app.py
@@ -42,9 +42,24 @@ def create_task():
     
     return task_schema.jsonify(new_task)
 
+@app.route('/tasks/<id>', methods = ["GET"])
+def get_task(id):
+    task = Task.query.get(id)
+    return task_schema.jsonify(task)
+
+@app.route('/tasks/<id>', methods = ["PUT"])
+def update_task(id):
+    task = Task.query.get(id)
+    task.title = request.json["title"]
+    task.description = request.json["description"]
+    db.session.commit()
+
+    return task_schema.jsonify(task)
+
+
 
 @app.route('/tasks', methods = ['GET'])
-def get_task():
+def get_tasks():
     all_tasks = Task.query.all()
     result = tasks_schema.dump(all_tasks)
     return jsonify(result)
