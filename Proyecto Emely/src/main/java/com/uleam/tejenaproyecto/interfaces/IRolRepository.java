7f2ad9157753b59a1542bd9a066eb893f635a230
Emely <106843681+EMELYTDFDGFHGJTRFRET@users.noreply.github.com>
2024-05-21 22:49:56 -0500

    Creacion Rol Repositorio

---
+++ Proyecto Emely/src/main/java/com/uleam/tejenaproyecto/interfaces/IRolRepository.java
@@ -0,0 +1,7 @@
+package com.uleam.tejenaproyecto.interfaces;
+
+import com.uleam.tejenaproyecto.modelo.Rol;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IRolRepository extends JpaRepository<Rol, Long> {
+}
