bd0723db04297b9befe36f0336a60ac269eddd18
Emely <106843681+EMELYTDFDGFHGJTRFRET@users.noreply.github.com>
2024-05-21 22:47:57 -0500

    Creacion Clase Equipo  Repository

---
+++ Proyecto Emely/src/main/java/com/uleam/tejenaproyecto/interfaces/IClaseEquipoRepository.java
@@ -0,0 +1,7 @@
+package com.uleam.tejenaproyecto.interfaces;
+
+import com.uleam.tejenaproyecto.modelo.ClaseEquipo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IClaseEquipoRepository extends JpaRepository<ClaseEquipo, Long> {
+}
