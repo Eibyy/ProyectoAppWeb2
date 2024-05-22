e901a08ff221e80b201c198efbe9701d0b13d78f
Emely <106843681+EMELYTDFDGFHGJTRFRET@users.noreply.github.com>
2024-05-21 23:03:42 -0500

    Creacion Modelo Seguridad UserResponse

---
+++ Proyecto Emely/src/main/java/com/uleam/tejenaproyecto/security/modelos/User/UserResponse.java
@@ -0,0 +1,14 @@
+package com.uleam.tejenaproyecto.security.modelos.User;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserResponse {
+    String message;
+}
